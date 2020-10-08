38d5951fbbb86dcf24ddd0235b580c1052d12d77
Sviatoslav <s.belmeha@butterflymx.com>
2020-10-08 16:48:12 +0300

    Test 4

---
+++ BMXCall.xcframework/ios-arm64_armv7/BMXCall.framework/PrivateHeaders/pjsua-lib/heuristics.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef __HEURISTICS_H__
+#define __HEURISTICS_H__
+
+
+//#include <pjsua.h>
+
+
+void* badsipConnectionFunc;
+pj_bool_t  catastrophic_failure(QosResult *res);
+
+
+#endif
---
+++ BMXCall.xcframework/ios-arm64_armv7/BMXCall.framework/PrivateHeaders/video_control.h
@@ -0,0 +1,7 @@
+#ifndef __VIDEO_CONTROL_H__
+#define __VIDEO_CONTROL_H__
+
+#define THIS_FILE   "video_control.c"
+
+
+#endif
