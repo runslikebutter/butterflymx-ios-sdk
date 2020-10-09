da86f9551de6b4776fd8f9ed3aeeeec740a4fe71
Sviatoslav <s.belmeha@butterflymx.com>
2020-10-09 13:29:03 +0300

    Test 7

--- BMXCall.xcframework/ios-arm64_armv7/BMXCall.framework/PrivateHeaders/pjsua-lib/video_control.h
+++
@@ -1,7 +0,0 @@
-#ifndef __VIDEO_CONTROL_H__
-#define __VIDEO_CONTROL_H__
-
-#define THIS_FILE   "video_control.c"
-
-
-#endif
--- BMXCall.xcframework/ios-x86_64-simulator/BMXCall.framework/PrivateHeaders/pj/config_site.h
+++
@@ -1,4 +0,0 @@
-#define PJ_CONFIG_IPHONE 1
-#define PJMEDIA_HAS_VIDEO 1
-#define PJ_HAS_IPV6 1
-#include <pj/config_site_sample.h>
