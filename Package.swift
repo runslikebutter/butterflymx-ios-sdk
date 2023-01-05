7de17440dd88a74827c7471bd527b1865cac9ed6
Sviatoslav <s.belmeha@butterflymx.com>
2023-01-05 12:12:55 +0200

    add spm

---
+++ Package.swift
@@ -0,0 +1,23 @@
+var package = Package(
+    name: "BMXCore",
+    platforms: [
+        .ios(.v12)
+    ],
+    products: [
+        .library(
+            name: "BMXCore",
+            targets: [ "BMXCore" ]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
+        .package(name: "OAuthSwift", url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/infinum/Japx.git", .upToNextMajor(from: "4.0.0"))
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BMXCore",
+            url: "https://github.com/runslikebutter/butterflymx-ios-sdk/raw/BMXCore_2.3/BMXCore.zip",
+            checksum: "4f6a397cf3256d1686b7d9c6d9b1a6d93edea0415d4fb628c764527d7f107c2d"
+        )
+    ]
+)
