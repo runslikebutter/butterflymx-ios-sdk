04b3a7761c0ee8af370e1944a7147b63abcf9a4c
Sviatoslav <s.belmeha@butterflymx.com>
2023-01-05 12:26:22 +0200

    upd

--- Package.swift
+++ Package.swift
@@ -1,3 +1,8 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 var package = Package(
     name: "BMXCore",
     platforms: [
